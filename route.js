8ed2837d8638462fdf866546d144d980dcab1a50
QuagKai <119025914+QuagKai@users.noreply.github.com>
2023-09-08 15:26:58 +0700

    - Add functions for product interaction

--- route.js
+++ route.js
@@ -1,13 +1,49 @@
 const express = require('express');
-const { showAllProducts } = require('./src/controller/productController')
+const { showAllProducts, showAProduct, createProduct, updateProduct, deleteProduct } = require('./src/controller/productController')
 const router = express.Router();
 
 router.get('/api/homepage', (req, res) => {
-    res.send('This is a homepage')
+    res.send('This is a homepage');
 })
 
-router.get('/api/manager/showAllProducts', showAllProducts, (req, res) => {
-    console.log('showProduct route end');
+//All routes for product interaction (client role)
+router.get('/api/browse/product/$id', showAProduct, (req, res) => {
+    console.log("browsingAProduct route end")
+})
+
+router.post('/api/browse/$nameCate/product/$id/addToCart', addToCart, (req, res) => {
+    res.redirect('/api/browse/product/$id')
+    console.log("addToCart route end")
+})
+
+router.post('/api/browse/$nameCate/product/$id/removeOutCart', removeOutCart, (req, res) => {
+    res.redirect('/api/browse/product/$id')
+    console.log("removeOutCart route end")
+})
+
+router.get('/api/browse/all/', showAllProducts, (req, res) => {
+    console.log("browsingAllProduct route end")
+})
+
+//All routes for product management
+router.get('/api/manager/product/showAll', showAllProducts, (req, res) => {
+    console.log('showAllProduct route end');
+})
+
+router.get('/api/manager/product/create', createProduct, (req, res) => {
+    console.log('createProduct route end');
+})
+
+router.get('/api/manager/product/$id', showAProduct, (req, res) => {
+    console.log("showAProduct route end");
+})
+
+router.get('/api/manager/product/$id/update', updateProduct, (req, res) => {
+    console.log("updateProduct route end");
+})
+
+router.get('/api/manager/product/$id/delete', deleteProduct, (req, res) => {
+    console.log("deleteProduct route end");
 })
 
 module.exports = router
--- src/controller/productController.js
+++ src/controller/productController.js
@@ -7,4 +7,65 @@ const showAllProducts = async(req, res, next) => {
     next()
 }
 
-module.exports = { showAllProducts }
+const showAProduct = async(req, res, next) => {
+    await Product.findById(req.params.id)
+    .then((p) => {
+        console.log("Found a product");
+        res.json(p)
+    })
+    .catch((err) => {
+        console.log("Failed to update");
+        throw err
+    })
+    next()
+}
+
+const createProduct = async(req, res, next) => {
+    await Product.create(req.body)
+    .then(() => console.log("Created product successfully"))
+    .catch((err) => {
+        console.log("Failed to created")
+        throw err
+    })
+    await Product.save()
+    .then(() => console.log("Product collection saved"))
+    .catch((err) => {
+        console.log("Product colletion failed to save")
+        throw err
+    })
+    next()
+}
+
+const updateProduct = async(req, res, next) => {
+    await Product.findByIdAndUpdate(req.body._id)
+    .then(() => console.log("Update Successfully"))
+    .catch((err) => {
+        console.log("Failed to update")
+        throw err
+    })
+    await Product.save()
+    .then(() => console.log("Product collection saved"))
+    .catch((err) => {
+        console.log("Product colletion failed to save")
+        throw err
+    })
+    next()
+}
+
+const deleteProduct = async(req, res, next) => {
+    await Product.findByIdAndDelete(req.body._id)
+    .then(() => console.log("Successfully delete"))
+    .catch((err) => {
+        console.log("Failed to delete")
+        throw err
+    })
+    await Product.save()
+    .then(() => console.log("Product collection saved"))
+    .catch((err) => {
+        console.log("Product colletion failed to save")
+        throw err
+    })
+    next()
+}
+
+module.exports = { showAllProducts, showAProduct, createProduct, updateProduct, deleteProduct }
